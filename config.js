e605b8e61f9364dfd6ef023967c24f4231228238
Joseph Odion <126256678+Joeboymedia@users.noreply.github.com>
2024-06-08 04:28:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,9 @@ const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
 
 const ownerlist = ownervb.split(';');
 
-global.owner = [];
+global.owner = [
+    ['2349157999879', 'Joeboy', true],
+    ];
 for (let i = 0; i < ownerlist.length; i += 2) {
     const owner = [
         ownerlist[i],            
@@ -23,9 +25,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['2349157999879', '32460248586']
+global.prems = ['2349157999879', '32460248586', '919398758484']
+global.allowed = ['2349157999879', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
@@ -71,9 +73,9 @@ global.APIKeys = {
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
+global.packname = 'Joeboy┃ᴮᴼᵀ'
+global.author = 'ˢᵉⁿˢᵉⁱ'
+global.menuvid = 'https://ugc.production.linktr.ee/Ol5D4MdLRb2eapp83Qz8_0001-172661438693669646.png?io=true&size=avatar-v3_0'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/guru.json\n'
 global.dygp = 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT'
